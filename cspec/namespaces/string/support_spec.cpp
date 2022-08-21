3188bd15d5d9d1d8b673f4709670ccb0a5ba896b
andrey moura <andreymds88@gmail.com>
2022-08-21 18:03:20 -0300

    Tests converted to new version

--- cspec/namespaces/string/support_spec.cpp
+++ cspec/namespaces/string/support_spec.cpp
@@ -51,28 +51,28 @@ std::map<int, std::string> romanNumeralsMap =
 
 cspec_describe("uva::support",
 
-    describe("join",
+    context("join",
         it("should join a map",[](){
             auto joined_roman_numerals = uva::string::join(romanNumeralsMap, [](const auto& number_roman) {
                 return std::format("{}={}", number_roman.first, number_roman.second);
             });
 
-            expect(joined_roman_numerals).to() << eq(std::vector<std::string>{"1=I", "2=II", "3=III", "4=IV", "5=V",
+            expect(joined_roman_numerals).to eq(std::vector<std::string>{"1=I", "2=II", "3=III", "4=IV", "5=V",
                                                                               "6=VI", "7=VII", "8=VIII", "9=IX", "10=X" });
         })
     )
 
-    describe("select",
+    context("select",
         it("should only include strings who starts with a", [](){
             std::vector<std::string> only_starts_with_a_names = uva::string::select(person_names, [](const std::string& str) {
                 return str.starts_with('A');
             });
 
-            expect(only_starts_with_a_names).to() << eq(std::vector<std::string>({"Adams"}));
+            expect(only_starts_with_a_names).to eq(std::vector<std::string>({"Adams"}));
         })
     )
 
-    describe("map",
+    context("map",
         it("should map integers to strings", [](){
             std::vector<int> firstTenNumbers = uva::faker::sequence::from_to(0, 10);
 
@@ -80,7 +80,7 @@ cspec_describe("uva::support",
                 return std::to_string(i);
             });
 
-            expect(vec).to() << eq(std::vector<std::string>({"0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }));
+            expect(vec).to eq(std::vector<std::string>({"0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }));
         })
     )
 );
